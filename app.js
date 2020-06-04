c178e22de0be876957224e28ce823bcba2dafedb
Bryce Speirs <baspeirs@gmail.com>
2020-06-04 12:22:35 -0400

    used given conversion file to put object information on the page

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const fs = require("fs");
 const mangagerQuestions = require("./managerQuestions");
 const engQuestions = require("./eng-questions");
 const intQuestions = require("./int-questions");
-// const intQuestions = 
+const util = require("util")
+
+const asyncWriteFile = util.promisify(fs.writeFile)
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -16,15 +18,18 @@ const render = require("./htmlRenderer");
 
 // empty array to push employee id numbers
 let employeeIdArray = [];
-let employeeList = [];
+let employees = [];
+// let managerArray = [];
+// let engineerArray = [];
+// let internArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 async function engineerInfo() {
     try {
         const response = await inquirer.prompt(engQuestions)
-        const engineer = new Manager(response.engineerName, response.engineerID, response.engineerEmail, response.engineerGitHub);
-        employeeList.push(engineer)
+        const engineer = new Engineer(response.engineerName, response.engineerID, response.engineerEmail, response.engineerGitHub);
+        employees.push(engineer)
         if (response.addEmployee === "Engineer") {
             engineerInfo()
         }
@@ -43,7 +48,7 @@ async function internInfo() {
     try {
         const response = await inquirer.prompt(intQuestions)
         const intern = new Intern(response.internName, response.internID, response.internEmail, response.internSchool);
-        employeeList.push(intern)
+        employees.push(intern)
         if (response.addEmployee === "Engineer") {
             engineerInfo()
         }
@@ -61,7 +66,8 @@ async function internInfo() {
 
 async function renderHtml() {
     try {
-        console.log(employeeList)
+        const newHtml = await render(employees);
+        await asyncWriteFile(outputPath, newHtml)
     }
     catch(err) {
         console.log(err)
@@ -71,17 +77,16 @@ async function renderHtml() {
 async function askQuestions() {
     try {
         const response = await inquirer.prompt(mangagerQuestions)
+        // const { managerName, managerID,  }
         employeeIdArray.push(response.managerID)
-        console.log(employeeIdArray);
         const manager = new Manager(response.managerName, response.managerID, response.managerEmail, response.managerNumber);
-        employeeList.push(manager)
+        employees.push(manager)
     if (response.addEmployee === "Engineer") {
         engineerInfo()
     }
     else if (response.addEmployee === "Intern") {
         internInfo()
     }
-    
     }
     catch(err) {
         console.log(err)
@@ -89,22 +94,3 @@ async function askQuestions() {
 } 
 
 askQuestions()
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
